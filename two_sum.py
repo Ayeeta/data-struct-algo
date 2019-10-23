5b8d702f4a4397a0e90093d6f14da8c1b42f2c44
Elijah <elijate@gmail.com>
2019-10-23 23:37:15 +0300

    Two Sum LeetCode

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        x = target - nums[i]
+        if x in nums[i+1:]:
+            return [i, nums[i+1:].index(x)+i+1]
+    
+    
+
+l = [3,2,4]
+t = 6
+
+print(two_sum(l, t))
+
