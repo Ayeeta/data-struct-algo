6e595024adc64cbc8b17135a6fa29cf121a087f8
Elijah <elijate@gmail.com>
2019-10-23 22:00:56 +0300

    Add function to traverse the linkedlist

--- linkedList/singlyLinkedList/LinkedList.py
+++ linkedList/singlyLinkedList/LinkedList.py
@@ -1,5 +1,14 @@
+from Node import Node
+
 class LinkedList:
     def __init__(self):
         self.start_node = None
 
-    
+    def traverse(self):
+        if self.start_node is None:
+            return "LinkedList is empty"
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item, " ")
+                n = n.ref
