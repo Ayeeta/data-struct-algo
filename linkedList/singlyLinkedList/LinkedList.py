f40fcab65b9422fa8edd3575dc15201c3664ca19
Elijah <elijate@gmail.com>
2019-10-23 22:06:41 +0300

    Add insert_at_start function

--- linkedList/singlyLinkedList/LinkedList.py
+++ linkedList/singlyLinkedList/LinkedList.py
@@ -12,3 +12,8 @@ class LinkedList:
             while n is not None:
                 print(n.item, " ")
                 n = n.ref
+    
+    def insert_at_start(self, data):
+        new_node = Node(data)
+        new_node.ref = self.start_node
+        self.start_node = new_node
