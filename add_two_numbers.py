8299ccd78d4eb032e79507ccd9392008105b861b
Elijah <elijate@gmail.com>
2019-10-26 06:53:27 +0300

    Add ListNode Class

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -5,10 +5,10 @@ print(x)
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
         def addTwoNumbers(self, l1, l2 ,c = 0):
