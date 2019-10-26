319bce09ab49fbcc7c1d5fceb5d3d8c4fe8b31a9
Elijah <elijate@gmail.com>
2019-10-26 07:26:20 +0300

    return the length of the longest substring

---
+++ longest_string.py
@@ -0,0 +1,18 @@
+# Given a string, find the length of the longest substring without repeating characters.
+
+def longest_substring(s):
+    s_dict = {}
+    max_s = curr_max = start = 0
+
+    for i, j in enumerate(s):
+        if j in s_dict and s_dict[j] >= start:
+            max_s = max(max_s, curr_max)
+            curr_max = i - s_dict[j]
+            start = s_dict[j] + 1
+        else:
+            curr_max += 1
+        s_dict[j] = i
+    return max_s
+
+
+print(longest_substring("abcabcbb"))
