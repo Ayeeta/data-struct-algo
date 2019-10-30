afda403db09754749dbbe2252b295017c6bba4d2
Elijah <elijate@gmail.com>
2019-10-30 09:13:25 +0300

    Return the longest palindrome in a given string

--- longest_palindromic_string.py
+++ longest_palindromic_string.py
@@ -1,6 +1,13 @@
 def longestPalindrome(s):
     res = [s[i:j] for i in range(len(s)) for j in range(i+1 , len(s)+1)]
-    return res
+    pal_list = {}
+    #length_s = 0
+    for word in res:
+        if word == word[::-1]:
+            pal_list[len(word)] = word
+    
+    last_key =  list(pal_list.keys())[-1]
+    return pal_list[last_key]
 
 
-print(longestPalindrome('geeks'))
+print(longestPalindrome('cbbd'))
