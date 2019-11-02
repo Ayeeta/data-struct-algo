d7973ddfe2d68dceec7c013923846abd044e0618
Elijah <elijate@gmail.com>
2019-11-02 08:42:11 +0300

    remove duplicates in-place from a sorted array

--- remove_duplicates_list.py
+++ remove_duplicates_list.py
@@ -1,15 +1,12 @@
-nums = [0]
-
 def remove_duplicates(nums):
-    nums.sort()
-    count_len = 1
-    curr = 0
     if len(nums) == 0:
         return 0
-    for i in range(1, len(nums)):
-        if nums[i] != nums[curr]:
-            count_len += 1
-        nums[curr] = nums[i]
-    return count_len
+    i = 0
+    for elem in range(1, len(nums)):
+        if nums[elem] != nums[i]:
+            i += 1
+            nums[i] = nums[elem]
+            
+    return i + 1
 
-print(remove_duplicates(nums))
+print(remove_duplicates([0,0,1,1,1,2,2,3,3,4]))
