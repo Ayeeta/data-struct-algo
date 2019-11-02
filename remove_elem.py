041631e7ba4aab36f032c303e476f5749975d4ee
Elijah <elijate@gmail.com>
2019-11-02 08:25:15 +0300

    Remove duplicates from a list in-place soln 2

--- remove_elem.py
+++ remove_elem.py
@@ -1,7 +1,15 @@
+#did not pass tests on leetcode
 def remove_element(arr, x):
     new_arr = [elem for elem in arr if elem != x]
-    return new_arr
+    return len(new_arr)
 
 
+def remove_element2(arr, x):
+    i = 0
+    for elem in range(len(arr)):
+        if arr[elem] != x:
+            arr[i] = arr[elem]
+            i += 1
+    return i
 
-print(remove_element([3,2,2,3], 3))
+print(remove_element2([3,2,2,3], 3))
