a1e4c8a75d2783ec90ddef87ef986950b57a7cb1
Elijah <elijate@gmail.com>
2019-11-05 01:47:02 +0300

    Use eval() method

--- stacks.py
+++ stacks.py
@@ -18,10 +18,24 @@ def postfix_expr(strs):
             if i == "*":
                 stack_.append(int(a) * int(b))
     return stack_[len(stack_)-1]
+
+
+
+def postfix_expr2(strs):
+    stack_ = []
+    math_operators = "+-/*"
+    for i in strs:
+        if i.isdigit():
+            stack_.append(i)
+        if i in math_operators:
+            b = stack_.pop()
+            a = stack_.pop()
+            stack_.append(str(eval(a + i + b)))
+    return stack_[len(stack_)-1]
             
         
 
 
-print(postfix_expr("2 3 1 * + 9 -"))
+print(postfix_expr2("2 3 1 * + 9 -"))
 
 
