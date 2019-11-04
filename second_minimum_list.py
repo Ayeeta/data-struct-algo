3769fbdc2472b88a7c97052aa90a4fc9c5dd4100
Elijah <elijate@gmail.com>
2019-11-05 00:27:02 +0300

    Return the first non repeating value

--- second_minimum_list.py
+++ second_minimum_list.py
@@ -6,3 +6,13 @@ def second_minimum(arr):
 print(second_minimum([8,2,4,1,6,4,3,7,8,9]))
 
 
+#first non repeating elements of an array
+def non_repeating(arr):
+    elem = 0
+    for i in range(len(arr)):
+        elem = arr.count(arr[i])
+        if elem == 1:
+            return arr[i]
+                
+
+print(non_repeating([8,3,2,4,1,2,8,6,9,2]))
