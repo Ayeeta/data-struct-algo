3bc52bec93e9f72c6432890accd14c0851d8fc0c
Elijah <elijate@gmail.com>
2019-11-05 12:56:41 +0300

    Balanced parenthesis problem based on matching parenthesis pos

--- stacks.py
+++ stacks.py
@@ -58,8 +58,31 @@ def balanced_parenthesis(parenthesis):
             return "balanced"
         else:
             return "Unbalanced"
+
+#balanced parenthesis 2 - matching based on positiions
+
+# Python3 code to Check for  
+# balanced parentheses in an expression 
+open_list = ["[","{","("] 
+close_list = ["]","}",")"] 
+  
+# Function to check parentheses 
+def check(myStr): 
+    stack = [] 
+    for i in myStr: 
+        if i in open_list: 
+            stack.append(i) 
+        elif i in close_list: 
+            pos = close_list.index(i) 
+            if ((len(stack) > 0) and
+                (open_list[pos] == stack[len(stack)-1])): 
+                stack.pop() 
+            else: 
+                return "Unbalanced"
+    if len(stack) == 0: 
+        return "Balanced"
             
 
-print(balanced_parenthesis("){}{})(][]"))
-
+#print(balanced_parenthesis("){}{})(][]"))
+print(check("){}{})(][]"))
         
