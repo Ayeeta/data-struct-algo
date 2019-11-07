2ed3ea72f1f427bc3b92cdbbe4d20528c844994b
Elijah <elijate@gmail.com>
2019-11-07 09:58:43 +0300

    Rotate a matrix 90 degrees

---
+++ rotate_matrix.py
@@ -0,0 +1,15 @@
+def rotate_90_degrees(matrix):
+    n = len(matrix) - 1
+    d = 0   # diagonal
+    
+    while d < (n + 1) // 2:     # loop through 1st half of diagonal
+        
+        for i in range(d, n - d):   # loop through row
+            
+            temp = matrix[d][i]
+            matrix[d][i] = matrix[n - i][d]
+            matrix[n - i][d] = matrix[n - d][n - i]
+            matrix[n - d][n - i] = matrix[i][n - d]
+            matrix[i][n - d] = temp
+        
+        d += 1
