1e06ff152e58a921870e59cc43f026ae4a43f297
Elijah <elijate@gmail.com>
2019-11-08 09:14:46 +0300

    Add both methods for int permutations

--- int_permutation.py
+++ int_permutation.py
@@ -1,6 +1,14 @@
 import itertools
-
+# faster 28ms
 def int_perm(int_list):
+    set_lists =  list(itertools.permutations(int_list))
+    l = []
+    for set_list in set_lists:
+        l.append(list(set_list))
+    return l
+
+#slower by 36ms
+def int_perm2(int_list):
     return [list(x) for x in list(itertools.permutations(int_list))]
 
 
