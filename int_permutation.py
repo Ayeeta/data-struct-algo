be325f65df0481c4e27f820506f6f012adecafe6
Elijah <elijate@gmail.com>
2019-11-08 09:12:45 +0300

    int list permutation using list comprehenssions

--- int_permutation.py
+++ int_permutation.py
@@ -1,11 +1,7 @@
 import itertools
 
 def int_perm(int_list):
-    set_lists =  list(itertools.permutations(int_list))
-    l = []
-    for set_list in set_lists:
-        l.append(list(set_list))
-    return l
+    return [list(x) for x in list(itertools.permutations(int_list))]
 
 
 print(int_perm([1,2]))
