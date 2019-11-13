59ecb736c54c898cde4f1fb8bd36f3b0baa0b4a6
Elijah <elijate@gmail.com>
2019-11-13 08:15:41 +0300

    Matching parenthesis 2

--- parenthesis.py
+++ parenthesis.py
@@ -1,4 +1,4 @@
-#simpler problem
+#simpler problem - assume starting open parenthesis
 def parenthesis_problem(s):
     left = 0
     for p in s:
@@ -12,4 +12,19 @@ def parenthesis_problem(s):
         return "Invalid"
 
 
-print(parenthesis_problem(")((()))("))
+# print(parenthesis_problem(")((()))("))
+
+def parenthesis_problem2(s):
+    bracket_map = {"(": ")", "[": "]",  "{": "}"}
+    open_par = set(["(", "[", "{"])
+    stack = []
+    for i in s:
+        if i in open_par:
+            stack.append(i)
+        elif stack and i == bracket_map[stack[-1]]:
+                stack.pop()
+        else:
+            return False
+    return stack == []
+
+print(parenthesis_problem2("{()}"))
