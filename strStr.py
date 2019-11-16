4c2ea4eebad62815240b8ea9a1b6f8161ce7a00a
Elijah <elijate@gmail.com>
2019-11-16 11:06:22 +0300

    Find 1st occurrence of needle in haystack soln2 - leetcode

--- strStr.py
+++ strStr.py
@@ -8,4 +8,11 @@ def str_occ(haystack, needle):
     if  needle not in haystack:
         return -1
     
-print(str_occ("aaaaa", "bba"))
+# print(str_occ("aaaaa", "bba"))
+
+def str_occ2(haystack, needle):
+    if needle == "":
+        return 0
+    return haystack.find(needle)
+
+print(str_occ2("hello", "llo"))
