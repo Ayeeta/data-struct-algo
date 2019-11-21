1a1eacd4e7b081c4e2e3601e7f8b8def666796a2
Elijah <elijate@gmail.com>
2019-11-22 01:34:19 +0300

    Return non repeating number in a list soln3

--- single_num.py
+++ single_num.py
@@ -14,4 +14,14 @@ def single_number2(nums):
             dups.remove(i)
     return dups.pop()
 
-print(single_number2([2,2,1]))
+def single_number3(nums):
+    hash_t = {}
+    for i in nums:
+        try:
+            hash_t.pop(i)
+        except:
+            hash_t[i] = 1
+    return hash_t.popitem()[0]
+
+
+print(single_number3([2,2,1]))
