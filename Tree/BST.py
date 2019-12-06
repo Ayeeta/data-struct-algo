4619117cfdeb6cb893582f1c411f3f4b723f726f
Elijah <elijate@gmail.com>
2019-12-06 09:16:30 +0300

    Boiler plate for BST Tree and Node class

---
+++ Tree/BST.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.rightChild = None
+        self.leftChild = None
+
+class Tree:
+    def __init__(self):
+        self.root = None
