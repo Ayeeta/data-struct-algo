4d36213c01d2b31046c569764b732a49d515a744
Elijah <elijate@gmail.com>
2019-12-06 09:23:57 +0300

    Add insert funtionality to BST

--- Tree/BST.py
+++ Tree/BST.py
@@ -4,6 +4,29 @@ class Node:
         self.rightChild = None
         self.leftChild = None
 
+    def insert(self, data):
+        if self.value == data:
+            return False
+        elif self.value > data:
+            if self.leftChild:
+                self.leftChild.insert(data)
+            else:
+                self.leftChild = Node(data)
+                return True
+        else:
+            if self.rightChild:
+                self.rightChild.insert(data)
+            else:
+                self.rightChild = Node(data)
+                return True
+
 class Tree:
     def __init__(self):
         self.root = None
+
+    def insert(self, data):
+        if self.root:
+            self.root.insert(data)
+        else:
+            self.root = Node(data)
+            return True
