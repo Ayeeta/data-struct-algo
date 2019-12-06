74f9e8811f503b14319775dc36bed23be37fcf46
Elijah <elijate@gmail.com>
2019-12-06 09:49:37 +0300

    Add Find method to search for a value in the BST

--- Tree/BST.py
+++ Tree/BST.py
@@ -19,6 +19,21 @@ class Node:
             else:
                 self.rightChild = Node(data)
                 return True
+    
+    def find(self, data):
+        if self.value == data:
+            return True
+        elif self.value > data:
+            if self.leftChild:
+                return self.leftChild.find(data)
+            else:
+                return False
+        else:
+            if self.rightChild:
+                return self.rightChild.find(data)
+            else:
+                return False
+
 
 class Tree:
     def __init__(self):
@@ -30,3 +45,9 @@ class Tree:
         else:
             self.root = Node(data)
             return True
+    
+    def find(self, data):
+        if self.root:
+            return self.root.find(data)
+        else:
+            return False
