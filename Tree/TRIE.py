c9901bb5dee6dd35bcccfdd2fd1db9a423e7f4cd
Elijah <elijate@gmail.com>
2019-12-07 15:57:57 +0300

    Add search method to class Trie

--- Tree/TRIE.py
+++ Tree/TRIE.py
@@ -9,4 +9,16 @@ class Trie:
                 cur[ch]={}
             cur = cur[ch]
         cur['*'] = True
+
+    def search(self, word):
+        cur = self.head
+        for ch in word:
+            if ch not in cur:
+                return False
+            cur = cur[ch]
+        
+        if '*' in cur:
+            return True
+        else:
+            return False
         
