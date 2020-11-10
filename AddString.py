449ff6ea10ee5debf3c93dcdcf0f744ce648ab56
Elijah <elijate@gmail.com>
2020-11-10 13:53:05 +0300

    Refactored to use eval()

--- AddString.py
+++ AddString.py
@@ -1,4 +1,9 @@
 def addString(num1, num2):
     return str(int(num1)+ int(num2))
 
-print(addString("5", "4"))
+#Without directly converting to int use eval()
+
+def addString2(a,b):
+    return str(eval(a+'+'+b))
+
+print(addString2("5", "4"))
